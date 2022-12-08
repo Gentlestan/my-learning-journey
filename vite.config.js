09267d7f5ce4e46205e690abfe444323a2adf30d
Gentlestan <81222819+Gentlestan@users.noreply.github.com>
2022-12-08 17:56:11 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,20 @@
 import {defineConfig} from 'vite'
 
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        about: './about.html',
+        // ...
+        // List all files you want in your build
+      }
+    }
+  }
+})
+/*import {defineConfig} from 'vite'
+
 
 export default defineConfig({
 	plugins: []	
-})
+})*/
